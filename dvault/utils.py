00072a4cb1e1cdc3f11d439cc0270e944a448fbf
Derrick Karimi <derrick.karimi@gmail.com>
2022-06-12 12:50:25 -0400

    wip: debugging deployment

--- dvault/utils.py
+++ dvault/utils.py
@@ -1,10 +1,7 @@
 
 class dvault_update:
     uninstall_cmd = [ 'pip', 'uninstall', '-y', 'dvault' ],
-
     install_cmd = [ 'pip', 'install', 'git+ssh://git@github.com/AlwaysTraining/dvault.git' ]
-
-    reinstall_cmds = [ uninstall, install ]
-
+    reinstall_cmds = [ uninstall_cmd, install_cmd ]
     entry_point = reinstall_cmds
 
