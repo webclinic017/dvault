ed1fd2eb205efb9f8232a80ce9be4b22dc39d57f
Derrick Karimi <derrick.karimi@gmail.com>
2022-07-07 23:13:30 -0400

    new charts

--- dvault/charts.py
+++ dvault/charts.py
@@ -8,6 +8,11 @@ class dvine_chart_orders:
             '--chart-type', 'orders',
             '--with-order-status', 'filled' ]
 
+class dvine_chart_accounts:
+    entry_point_base = dvine_chart.entry_point_base + [
+            '--chart-type', 'accounts'
+            ]
+
 class dvine_chart_all_returns:
     entry_point_base = dvine_chart_orders.entry_point_base + [
             '--orders-max-spam', 1 ]
@@ -36,6 +41,14 @@ class dvine_us_equity_3Pct_all_returns(dvine_chart_us_equity_3Pct):
 class dvine_us_equity_3Pct_recent_returns(dvine_chart_us_equity_3Pct):
     entry_point = dvine_chart_recent_returns.entry_point_base +  dvine_chart_us_equity_3Pct.base_args
 
+class dvine_us_equity_3Pct_performance(dvine_chart_accounts):
+    entry_point = \
+            dvine_chart_accounts.entry_point_base + \
+            dvine_chart_us_equity_3Pct.bot.strat.base_args + \
+            dvine_chart_us_equity_3Pct.base_args + [
+                    '--from-date', '2022-06-08T00:00:00',
+                    '--accounts-floor', 75000.00]
+
 class dvine_chart_us_equity_5Pct:
     bot = bots.dvine_us_equity_5Pct
     discord_webhook_url = "https://discord.com/api/webhooks/985015648923029544/tkru1WEjUkW3M_j1MrXOUQuQXZpbr0O6I7g84xyUFEcvfFbLlXDhnfpoVjDS7FwofdFc"
@@ -54,3 +67,11 @@ class dvine_us_equity_5Pct_all_returns(dvine_chart_us_equity_5Pct):
 
 class dvine_us_equity_5Pct_recent_returns(dvine_chart_us_equity_5Pct):
     entry_point = dvine_chart_recent_returns.entry_point_base +  dvine_chart_us_equity_5Pct.base_args
+
+class dvine_us_equity_5Pct_performance(dvine_chart_accounts):
+    entry_point = \
+            dvine_chart_accounts.entry_point_base + \
+            dvine_chart_us_equity_5Pct.bot.strat.base_args + \
+            dvine_chart_us_equity_5Pct.base_args + [
+                    '--from-date', '2022-05-27T00:00:00',
+                    '--accounts-floor', 92500.00]
