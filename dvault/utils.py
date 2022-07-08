a77cb19428012c98a8e1cb782d713d45b763392f
Derrick Karimi <derrick.karimi@gmail.com>
2022-07-07 23:31:00 -0400

    fix

--- dvault/utils.py
+++ dvault/utils.py
@@ -1,6 +1,6 @@
 
 class dvault_update:
-    uninstall_cmd = [ 'pip', 'uninstall', '-y', 'dvault' ],
+    uninstall_cmd = [ 'pip', 'uninstall', '-y', 'dvault' ]
     install_cmd = [ 'pip', 'install', 'git+ssh://git@github.com/AlwaysTraining/dvault.git' ]
     reinstall_cmds = [ uninstall_cmd, install_cmd ]
     entry_point = reinstall_cmds
