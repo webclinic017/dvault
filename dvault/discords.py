182ea11f528a8aa3cd9ebe25e2220e37de1cde7a
Derrick Karimi <derrick.karimi@gmail.com>
2022-08-29 22:20:22 -0400

    new module

---
+++ dvault/discords.py
@@ -0,0 +1,10 @@
+class dvine_5pct:
+    webhook_url = "https://discord.com/api/webhooks/1013881029741776936/ma14mO-2DqSMHegrB66wd3QGd7CSLPlS5kB2d4Sl5I3WCeo_LuI9CA3PnEMsxPs4wHB0"
+class dvine_3pct:
+    webhook_url = "https://discord.com/api/webhooks/1013881635139227681/mnATXh0jitOZuNwGqytEhLF1mhwwle4fJcU4TWrcxlSUs5iFhk5_EMK7Y_u65v_1v7Oo"
+class dvine_2pct:
+    webhook_url = "https://discord.com/api/webhooks/1013881824231047218/PD7HP1l6zP-g6yDCOJsjDeELfThZp51lY7P59HipchA9tkHQXlgqMT_UFapl8VEDU_ef"
+class dmoon_adhoc:
+    webhook_url = "https://discord.com/api/webhooks/1013882859750183024/vcdAji_fSkPAChD1zTxXBpF6MOfBLGxNyLptW2I1MzVduyal6jKtJmk9kdE7uPJddXQt"
+class dstock_dspam:
+    webhook_url = "https://discord.com/api/webhooks/1013886668593442907/kdztwG1RS1KIKDZ2d8PxhLo--JifZAELNcqyoMRcDpYECf9VLsSuAPtFACKAW5leitaS"
